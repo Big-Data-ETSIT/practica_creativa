4e91d8502212b41a34b01782ab77547b7c01f3e3
dveni <28764301+dveni@users.noreply.github.com>
2020-11-20 09:29:41 +0100

    Script modified to create data directory

--- resources/download_data.sh
+++ resources/download_data.sh
@@ -5,11 +5,12 @@ ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_
 ABSOLUTE_DIR=$(dirname "${ABSOLUTE_PATH}")
 
 # Extract to Agile_Data_Code_2/data/on_time_performance.parquet, wherever we are executed from
-cd $ABSOLUTE_DIR/../data/
-curl -Lko ./simple_flight_delay_features.jsonl.bz2 http://s3.amazonaws.com/agile_data_science/simple_flight_delay_features.jsonl.bz2
+cd $ABSOLUTE_DIR/..
+mkdir data
+curl -Lko ./data/simple_flight_delay_features.jsonl.bz2 http://s3.amazonaws.com/agile_data_science/simple_flight_delay_features.jsonl.bz2
 
 # Get the distances between pairs of airports
-curl -Lko ./origin_dest_distances.jsonl http://s3.amazonaws.com/agile_data_science/origin_dest_distances.jsonl
+curl -Lko ./data/origin_dest_distances.jsonl http://s3.amazonaws.com/agile_data_science/origin_dest_distances.jsonl
 
 # Get the models to make ch08/web/predict_flask.py go
 cd $ABSOLUTE_DIR/..
