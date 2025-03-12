f03da67dcb710d6f8159e03dc2150a2e99ae0294
anmunoz <andres.max36@gmail.com>
2025-03-12 19:02:58 +0100

    Modify mongo index creation

--- resources/import_distances.sh
+++ resources/import_distances.sh
@@ -2,4 +2,4 @@
 
 # Import our enriched airline data as the 'airlines' collection
 mongoimport -d agile_data_science -c origin_dest_distances --file data/origin_dest_distances.jsonl
-mongo agile_data_science --eval 'db.origin_dest_distances.ensureIndex({Origin: 1, Dest: 1})'
+mongo agile_data_science --eval 'db.origin_dest_distances.createIndex({Origin: 1, Dest: 1})'
